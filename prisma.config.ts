25771e04f6db760ee9c806f3e2f7e951434c5696
kmdee8 <kmdee8@gmail.com>
2026-03-09 16:14:02 +0000

    Imports URL for migrations

--- prisma.config.ts
+++ prisma.config.ts
@@ -10,5 +10,7 @@ export default defineConfig({
   },
   datasource: {
     url: process.env["DATABASE_URL"] as string,
+    // Use for migrations
+    // url: process.env["DIRECT_URL"] as string,
   },
 });
